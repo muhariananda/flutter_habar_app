67b143f61da326e86eafab6f6094fcd1d64d1940
Muhammad Ariananda <m.ariananda079@gmail.com>
2023-04-13 01:28:02 +0800

    [FEAT] root - Add and configure App Route with go_route package

---
+++ lib/app_route.dart
@@ -0,0 +1,26 @@
+import 'package:go_router/go_router.dart';
+import 'package:news_list/news_list.dart';
+import 'package:news_repository/news_repository.dart';
+
+GoRouter configureRoutes({
+  required NewsRepository newsRepository,
+}) =>
+    GoRouter(
+      routes: [
+        GoRoute(
+          path: _PathConstants.tabConatinerPath,
+          builder: (context, state) => NewsListScreen(
+            newsRepository: newsRepository,
+            //TODO : Add detail route
+          ),
+        )
+      ],
+    );
+
+class _PathConstants {
+  const _PathConstants._();
+
+  static String get tabConatinerPath => '/';
+
+  static String get newsListPath => '${tabConatinerPath}news';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:component_library/component_library.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_news_app/app_route.dart';
+import 'package:key_value_storage/key_value_storage.dart';
+import 'package:news_api/news_api.dart';
+import 'package:news_repository/news_repository.dart';
 
 void main() {
   runApp(const NewsApp());
@@ -13,6 +17,18 @@ class NewsApp extends StatefulWidget {
 }
 
 class _NewsAppState extends State<NewsApp> {
+  final _keyValueStorage = KeyValueStorage();  
+  final _newsApi = NewsApi();
+  
+  late final _newsRepository = NewsRepository(
+    keyValueStorage: _keyValueStorage,
+    remoteApi: _newsApi,
+  );  
+
+  late final _route = configureRoutes(
+    newsRepository: _newsRepository,
+  );
+
   final _lightTheme = LightAppThemeData();
   final _darkTheme = DarkAppThemeData();
 
@@ -21,9 +37,11 @@ class _NewsAppState extends State<NewsApp> {
     return NewsTheme(
       lighTheme: _lightTheme,
       darkTheme: _darkTheme,
-      child: MaterialApp(
+      child: MaterialApp.router(
         theme: _lightTheme.materialThemeData,
         darkTheme: _darkTheme.materialThemeData,
+        routerDelegate: _route.routerDelegate,
+        routeInformationParser: _route.routeInformationParser,
       ),
     );
   }
