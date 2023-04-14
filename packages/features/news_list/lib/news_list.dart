479a6b70dc41f40c79f639d6e74f6313a391c664
Muhammad Ariananda <m.ariananda079@gmail.com>
2023-04-14 14:49:32 +0800

    [FEAT] news_list - Add screen for news list

---
+++ packages/features/news_list/lib/news_list.dart
@@ -0,0 +1,1 @@
+export 'src/news_list_screen.dart';
---
+++ packages/features/news_list/lib/src/news_list_screen.dart
@@ -0,0 +1,162 @@
+import 'package:component_library/component_library.dart';
+import 'package:domain_models/domain_models.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:news_list/src/bloc/news_list_bloc.dart';
+import 'package:news_list/src/widgets/filter_horizontal_list.dart';
+import 'package:news_list/src/widgets/news_page_list_view.dart';
+import 'package:news_repository/news_repository.dart';
+
+typedef ArticleSelected = void Function(Article selectedArticle);
+
+class NewsListScreen extends StatelessWidget {
+  const NewsListScreen({
+    required this.newsRepository,
+    this.onArticleSelected,
+    super.key,
+  });
+
+  final NewsRepository newsRepository;
+  final ArticleSelected? onArticleSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<NewsListBloc>(
+      create: (_) => NewsListBloc(
+        newsRepository: newsRepository,
+      ),
+      child: NewsListView(
+        onArticleSelected: onArticleSelected,
+      ),
+    );
+  }
+}
+
+class NewsListView extends StatefulWidget {
+  const NewsListView({
+    this.onArticleSelected,
+    super.key,
+  });
+
+  final ArticleSelected? onArticleSelected;
+
+  @override
+  State<NewsListView> createState() => _NewsListViewState();
+}
+
+class _NewsListViewState extends State<NewsListView> {
+  final PagingController<int, Article> _pagingController = PagingController(
+    firstPageKey: 1,
+  );
+
+  final TextEditingController _searchBarController = TextEditingController();
+
+  NewsListBloc get _bloc => context.read<NewsListBloc>();
+
+  @override
+  void initState() {
+    _pagingController.addPageRequestListener((pageNumber) {
+      final isSubsequentPage = pageNumber > 1;
+      if (isSubsequentPage) {
+        _bloc.add(
+          NewsListNextPageRequested(
+            pageNumber: pageNumber,
+          ),
+        );
+      }
+    });
+
+    _searchBarController.addListener(() {
+      _bloc.add(
+        NewsListSearchTermChanged(
+          _searchBarController.text,
+        ),
+      );
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pagingController.dispose();
+    _searchBarController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = NewsTheme.of(context)!;
+    return BlocListener<NewsListBloc, NewsListState>(
+      listener: (context, state) {
+        final searchBarText = _searchBarController.text;
+        final isSearching =
+            state.filter != null && state.filter is NewsListFilterBySearchTerm;
+
+        if (searchBarText.isNotEmpty && !isSearching) {
+          _searchBarController.text = '';
+        }
+
+        if (state.refreshError != null) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text(
+                'Error',
+              ),
+            ),
+          );
+        }
+
+        _pagingController.value = state.toPagingState();
+      },
+      child: SafeArea(
+        child: Scaffold(
+          body: GestureDetector(
+            onTap: () => _releaseFocus(context),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: theme.screenMargin,
+                  ),
+                  child: RoundedSearchBar(
+                    controller: _searchBarController,
+                  ),
+                ),
+                const FilterHorizontalList(),
+                Expanded(
+                  child: RefreshIndicator(
+                    onRefresh: () {
+                      _bloc.add(
+                        const NewsListRefreshed(),
+                      );
+                      final stateChangeFuture = _bloc.stream.first;
+                      return stateChangeFuture;
+                    },
+                    child: NewsPageListView(
+                      pagingController: _pagingController,
+                      onArticleSelected: widget.onArticleSelected,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _releaseFocus(BuildContext context) => FocusScope.of(context).unfocus();
+}
+
+extension on NewsListState {
+  PagingState<int, Article> toPagingState() {
+    return PagingState(
+      itemList: itemList,
+      nextPageKey: nextPage,
+      error: error,
+    );
+  }
+}
