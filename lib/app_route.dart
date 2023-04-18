a0075b646fb018f0263942f839093f2b06d32d6a
Muhammad Ariananda <m.ariananda079@gmail.com>
2023-04-18 15:56:46 +0800

    [FEAT] root - Add News detail route to app

--- lib/app_route.dart
+++ lib/app_route.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:domain_models/domain_models.dart';
 import 'package:go_router/go_router.dart';
+import 'package:news_detail/news_detail.dart';
 import 'package:news_list/news_list.dart';
 import 'package:news_repository/news_repository.dart';
 
@@ -12,9 +17,25 @@ GoRouter configureRoutes({
           builder: (context, state) => NewsListScreen(
             newsRepository: newsRepository,
             onArticleSelected: (selectedArticle) {
-              // ! TODO : Add Route to Detail Screen
+              context.push(
+                _PathConstants.newsDetailPath,
+                extra: jsonEncode(selectedArticle.toJson()),
+              );
             },
           ),
+        ),
+        GoRoute(
+          path: _PathConstants.newsDetailPath,
+          builder: (context, state) {
+            final articleJson = state.extra as String?;
+            final articleMap = jsonDecode(articleJson!) as Map<String, dynamic>;
+            final article = Article.fromJson(articleMap);
+
+            return NewsDetailScreen(
+              article: article,
+              newsRepository: newsRepository,
+            );
+          },
         )
       ],
     );
@@ -25,4 +46,6 @@ class _PathConstants {
   static String get tabConatinerPath => '/';
 
   static String get newsListPath => '${tabConatinerPath}news';
+
+  static String get newsDetailPath => '${tabConatinerPath}detail';
 }
