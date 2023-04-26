3448c61e6fd2d60b6fd82a161a65e97e85c70e09
Muhammad Ariananda <m.ariananda079@gmail.com>
2023-04-26 17:48:47 +0800

    [FEAT] bookmarks - Add Screen for bookamrks

---
+++ packages/features/bookmarks/lib/src/bookmarks_screen.dart
@@ -0,0 +1,81 @@
+import 'package:bookmarks/src/cubit/bookmarks_cubit.dart';
+import 'package:component_library/component_library.dart';
+import 'package:domain_models/domain_models.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_repository/news_repository.dart';
+
+typedef ArticleSelected = void Function(Article);
+
+class BookmarksScreen extends StatelessWidget {
+  const BookmarksScreen({
+    required this.newsRepository,
+    this.onSelectedArticle,
+    super.key,
+  });
+
+  final NewsRepository newsRepository;
+  final ArticleSelected? onSelectedArticle;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<BookmarksCubit>(
+      create: (_) => BookmarksCubit(
+        newsRepository: newsRepository,
+      ),
+      child: BookmarksView(
+        onSelectedArticle: onSelectedArticle,
+      ),
+    );
+  }
+}
+
+class BookmarksView extends StatelessWidget {
+  const BookmarksView({
+    this.onSelectedArticle,
+    super.key,
+  });
+
+  final ArticleSelected? onSelectedArticle;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = NewsTheme.of(context)!;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: Spacing.small,
+        horizontal: theme.screenMargin,
+      ),
+      child: BlocBuilder<BookmarksCubit, BookmarksState>(
+        builder: (context, state) {
+          if (state is BookmarksEmpty) {
+            return const EmptyItemsIndicator();
+          } else if (state is BookmarksLoaded) {
+            final articles = state.items;
+            return ListView.separated(
+              itemCount: articles.length,
+              separatorBuilder: (BuildContext context, int index) {
+                return const SizedBox(height: Spacing.mediumLarge);
+              },
+              itemBuilder: (BuildContext context, int index) {
+                final article = articles[index];
+                return ArticleSmallCard(
+                  title: article.title,
+                  author: article.author,
+                  publishedAt: article.publishedAt,
+                  imageUrl: article.urlToImage,
+                  onTap: () {
+                    onSelectedArticle?.call(article);
+                  },
+                );
+              },
+            );
+          } else {
+            return const Text('');
+          }
+        },
+      ),
+    );
+  }
+}
