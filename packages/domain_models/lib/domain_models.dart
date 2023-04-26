6fb88f2d2215ba5543d0bff83a2453b8d254a5d6
Muhammad Ariananda <m.ariananda079@gmail.com>
2023-04-27 00:22:33 +0800

    [FEAT] domain_models - Add dark mode preference model

--- packages/domain_models/lib/domain_models.dart
+++ packages/domain_models/lib/domain_models.dart
@@ -1,4 +1,6 @@
 export 'src/article.dart';
 export 'src/article_list_page.dart';
 export 'src/category.dart';
+export 'src/dark_mode_preference.dart';
+export 'src/localization_preference.dart';
 export 'src/source.dart';
---
+++ packages/domain_models/lib/src/localization_preference.dart
@@ -0,0 +1,4 @@
+enum LocalizationPreference {
+  en,
+  id,
+}
