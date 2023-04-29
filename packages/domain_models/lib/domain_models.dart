d169c845849884654638e92e111cc6bcbb2cf07b
Muhammad Ariananda <m.ariananda079@gmail.com>
2023-04-29 23:29:33 +0800

    [FEAT] domain_models - Add language preference model

--- packages/domain_models/lib/domain_models.dart
+++ packages/domain_models/lib/domain_models.dart
@@ -2,5 +2,5 @@ export 'src/article.dart';
 export 'src/article_list_page.dart';
 export 'src/category.dart';
 export 'src/dark_mode_preference.dart';
-export 'src/localization_preference.dart';
+export 'src/language_preference.dart';
 export 'src/source.dart';
--- packages/domain_models/lib/src/localization_preference.dart
+++
@@ -1,4 +0,0 @@
-enum LocalizationPreference {
-  en,
-  id,
-}
